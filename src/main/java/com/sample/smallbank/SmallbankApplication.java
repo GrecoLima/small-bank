7d444be39754d406a6609751900f1bc51bab4d91
grecolima <greco_lima@hotmail.com>
2023-09-01 17:54:48 +0100

    :sparkles: First Commit - Base Project

---
+++ src/main/java/com/sample/smallbank/SmallbankApplication.java
@@ -0,0 +1,13 @@
+package com.sample.smallbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallbankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmallbankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sample/smallbank/SmallbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.smallbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallbankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
